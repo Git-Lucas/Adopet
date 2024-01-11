44ab87233791b080e5f68ab998b4ec7dd9e9c75c
Lucas Gonçalves <lucas.goncalves@forpeople.io>
2024-01-11 17:43:24 -0300

    Add project files.

---
+++ Adopet.API/Dominio/Entity/Pet.cs
@@ -0,0 +1,19 @@
+﻿using Adopet.API.Util;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.API.Dominio.Entity
+{
+    internal class Pet
+    {
+        public Pet()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public TipoPet Tipo { get; set; }
+        public Cliente? Proprietario { get; set; }
+    }
+}
